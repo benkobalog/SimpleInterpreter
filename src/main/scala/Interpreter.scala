d060bb056cff315f4bd023bd003934179049e80b
benkobalog <benkobalog@gmail.com>
2017-10-20 23:14:22 +0200

    Refactor a bit

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -2,48 +2,10 @@ import scala.util.Try
 
 case class Interpreter(text: String) {
 
-  var pos: Int = 0
-  var currentToken: Token = _
-  var currentChar: Char = text(pos)
-
-  def error() = throw new Exception("Error parsing input")
-
-  def getInteger(): Token = {
-    val numbers = text.substring(pos).takeWhile(_.isDigit)
-    pos += numbers.length
-    Token("Integer", numbers)
-  }
-
-  def getNextToken(): Token = {
-    while (pos < text.length) {
-      currentChar = text(pos)
-      if (!currentChar.isWhitespace) {
-        return currentChar match {
-          case x if x.isDigit =>
-            getInteger()
-          case '+' | '-' | '*' | '/' =>
-            pos += 1
-            Token("Op", currentChar.toString)
-          case _ => error()
-        }
-      } else {
-        pos += 1
-      }
-    }
-    Token("EOF", "")
-  }
-
-  def eat(typ: String): Unit =
-    if (currentToken.typ == typ)
-      currentToken = getNextToken()
-    else {
-      println(currentToken)
-      error()
-    }
-
   def expr(): Int = {
+    val tokeniser = new Tokeniser(text)
     val tokens =
-      Stream.continually(getNextToken()).takeWhile(_.typ != "EOF").toList
+      Stream.continually(tokeniser.getNextToken).takeWhile(_.typ != "EOF").toList
 
     tokens.sliding(3, 2).foldLeft(tokens.head.value.toInt) {
       case (sum, List(_, op, x)) =>
--- src/main/scala/Tokeniser.scala
+++ src/main/scala/Tokeniser.scala
@@ -1,6 +1,34 @@
 case class Token(typ: String, value: String)
 
-object Tokeniser {
+class Tokeniser(val text: String) {
+  var pos: Int = 0
+  var currentToken: Token = _
+  var currentChar: Char = text(pos)
 
+  def error() = throw new Exception("Error parsing input")
 
+  def getInteger: Token = {
+    val numbers = text.substring(pos).takeWhile(_.isDigit)
+    pos += numbers.length
+    Token("Integer", numbers)
+  }
+
+  def getNextToken: Token = {
+    while (pos < text.length) {
+      currentChar = text(pos)
+      if (!currentChar.isWhitespace) {
+        return currentChar match {
+          case x if x.isDigit =>
+            getInteger
+          case '+' | '-' | '*' | '/' =>
+            pos += 1
+            Token("Op", currentChar.toString)
+          case _ => error()
+        }
+      } else {
+        pos += 1
+      }
+    }
+    Token("EOF", "")
+  }
 }
